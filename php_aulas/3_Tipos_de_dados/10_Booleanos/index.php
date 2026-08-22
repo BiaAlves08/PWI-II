656ded6f29d83afa689d5b1beb9068514d44b2e7
Bianca Santos <bianca1108alves@gmail.com>
2026-08-21 22:07:31 -0300

    Terminei, aleluia

--- php_aulas/3_Tipos_de_dados/10_Booleanos/index.php
+++ php_aulas/3_Tipos_de_dados/10_Booleanos/index.php
@@ -22,7 +22,19 @@ echo true;
 echo "<br>";
 echo false;
 
+if(true) {
+    echo "É verdadeiro! <br>";
+}
 
+if(5>2) { // true
+echo "É verdadeiro! <br>";
+}
+
+$podeEntrar = true;
+
+if($podeEntrar) {
+    echo "O usuário pode entrar";
+}
 
 
 ?>
