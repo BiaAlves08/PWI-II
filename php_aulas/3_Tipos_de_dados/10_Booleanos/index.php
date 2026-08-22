c4f9b7101596e94f2686a1a92a831065cc6a6c7a
Bianca Santos <bianca1108alves@gmail.com>
2026-08-21 21:22:24 -0300

    Uma parte da aula

---
+++ php_aulas/3_Tipos_de_dados/10_Booleanos/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booleanos em PHP</title>
+</head>
+<body>
+    
+<?php
+
+/*Booleanos
+O boolean é um tipo de dado que possui dois valores;
+TRUE = Verdadeiro
+False = Falso
+Alguns valores são considerados como falsos; 0,0.0 , 
+"0", [], NULL
+*/
+
+echo true;
+echo "<br>";
+echo false;
+
+
+
+
+?>
+
+
+</body>
+</html>
